026d1acafe76a46d775a6cc912de7e7d8e81c65f
Kevin Chung <63880429+kevhjc@users.noreply.github.com>
2022-02-03 11:01:56 -0800

    Add word count functionality

--- components/Submit.tsx
+++ components/Submit.tsx
@@ -1,10 +1,10 @@
-import { Menu, Transition } from '@headlessui/react'
-import { Fragment, useEffect, useRef, useState } from 'react'
+import { ChangeEvent, Fragment, useEffect, useRef, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import cn from 'classnames'
 
 export default function Submit() {
   const { data: session } = useSession()
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -14,7 +14,6 @@ export default function Submit() {
             aria-label="Submit a URL to the feed"
             placeholder="Submit a URL to the feed"
             type="url"
-            autoComplete="url"
             required
             disabled={session ? false : true}
             className={cn(
@@ -32,17 +31,29 @@ export default function Submit() {
             </button>
           ) : null}
           <input
-            aria-label="Add one or multiple hashtags"
-            placeholder="Add some #hashtags"
-            type="hashtag"
-            autoComplete="hashtag"
+            aria-label="Add a note"
+            placeholder="Add a note"
+            type="note"
+            maxLength={100}
             disabled={session ? false : true}
             className={cn(
               session
                 ? 'mt-2 block w-full rounded-md bg-white px-4 py-2 pr-32 text-gray-900 focus:border-blue-500 focus:ring-blue-500'
                 : 'mt-2 block w-full rounded-md bg-gray-200 px-4 py-2 pr-32'
             )}
+            onKeyUp={() => setCount(event.target.value.length)}
           />
+          <span
+            className={cn(
+              count <= 50
+                ? 'absolute right-1 top-12 flex h-8 items-end justify-end px-1 pt-2 font-medium text-gray-400 transition-all'
+                : count >= 75 && count <= 90
+                ? 'absolute right-1 top-12 flex h-8 items-end justify-end px-1 pt-2 font-bold text-orange-400 transition-all'
+                : 'absolute right-1 top-12 flex h-8 items-end justify-end px-1 pt-2 font-bold text-red-400 transition-all'
+            )}
+          >
+            {count} / 100
+          </span>
         </form>
         {!session ? (
           <p className="mt-4 font-mono text-sm text-gray-600">
