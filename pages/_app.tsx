487bb7f1afbc8f45d647256e2d7213a1e244364b
Kevin Chung <63880429+kevhjc@users.noreply.github.com>
2022-02-02 23:06:48 -0800

    Add SessionProvider wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app'
-import Layout from '../components/Layout'
+import { SessionProvider } from 'next-auth/react'
 
+import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <SessionProvider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
   )
 }
