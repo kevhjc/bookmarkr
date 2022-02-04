77a1caf7bcac72fcf48bd267e8dda3cf4fbeea32
Kevin Chung <63880429+kevhjc@users.noreply.github.com>
2022-02-03 16:12:16 -0800

    Remove console.log

--- pages/api/user.js
+++ pages/api/user.js
@@ -3,8 +3,6 @@ import { getSession } from 'next-auth/react'
 export default async (req, res) => {
   const session = await getSession({ req })
 
-  console.log(session)
-
   if (session) {
     res.send(session.user)
   } else {
