14b1364020b5d29b38f3aae259226a8a9a1a587d
Kevin Chung <63880429+kevhjc@users.noreply.github.com>
2022-02-04 14:26:13 -0800

    Add favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ export default function Home() {
   const meta = {
     title: 'Bookmarkr',
     description: `Social Bookmarking`,
-    image: '',
     type: 'website',
   }
 
@@ -16,15 +15,13 @@ export default function Home() {
     <Container>
       <Head>
         <title>{meta.title}</title>
+        <link href="../public/favicon.ico" rel="shortcut icon" />
         <meta name="robots" content="follow, index" />
         <meta content={meta.description} name="description" />
-        {/* <meta property="og:url" content={`///${router.asPath}`} /> */}
-        {/* <link rel="canonical" href={`///${router.asPath}`} /> */}
-        <meta property="og:type" content={meta.type} />
         <meta property="og:site_name" content="Bookmarkr" />
+        <meta property="og:type" content={meta.type} />
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
-        <meta property="og:image" content={meta.image} />
       </Head>
       <Submit />
       <Bookmark />
