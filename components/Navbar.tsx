2312b0f7596e8c2b31604b9b52320caad942b31f
Kevin Chung <63880429+kevhjc@users.noreply.github.com>
2022-02-04 14:25:51 -0800

    Update submit info

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import NextLink from 'next/link'
 import { useRouter } from 'next/router'
 import { useSession, signIn, signOut } from 'next-auth/react'
@@ -17,9 +18,7 @@ function NavItem({ href, text }: INavProps) {
     <NextLink href={href}>
       <a
         className={cn(
-          isActive
-            ? 'text-gray-darkest font-medium'
-            : 'font-medium text-gray-500',
+          isActive ? 'font-medium text-gray-900' : 'font-medium text-gray-500',
           'rounded p-2 text-gray-900 transition-all hover:bg-blue-100'
         )}
       >
@@ -29,15 +28,36 @@ function NavItem({ href, text }: INavProps) {
   )
 }
 
+function LogoNavItem({ href, text }: INavProps) {
+  const router = useRouter()
+  const isActive = router.asPath === href
+
+  return (
+    <NextLink href={href}>
+      <a
+        className={cn(
+          isActive
+            ? 'font-bold leading-snug tracking-tight text-gray-900'
+            : 'font-bold leading-snug tracking-tight text-gray-500',
+          'flex items-center gap-2 rounded p-2 text-gray-900 transition-all hover:bg-blue-100'
+        )}
+      >
+        <Image src="/logo.svg" alt="Bookmarkr Logo" width={20} height={20} />
+        {text}
+      </a>
+    </NextLink>
+  )
+}
+
 export default function Navbar() {
   const { data: session } = useSession()
 
   return (
-    <div className="mb-8 flex flex-wrap">
+    <div className="mb-8 flex flex-wrap text-lg">
       <div className="mb-4 w-full px-2 lg:w-1/2">
         <div className="text-grey-dark flex h-12 items-center justify-center lg:justify-start">
           <nav className="space-x-4 md:block">
-            <NavItem href="/" text="Bookmarkr" />
+            <LogoNavItem href="/" text="Bookmarkr" />
           </nav>
         </div>
       </div>
@@ -48,14 +68,14 @@ export default function Navbar() {
             {!session ? (
               <button
                 onClick={() => signIn('github')}
-                className="rounded bg-blue-600 p-1 px-3 font-bold text-white transition-all hover:bg-blue-700"
+                className="rounded bg-blue-600 p-1 px-3 text-white transition-all hover:bg-blue-700"
               >
                 Sign in
               </button>
             ) : (
               <button
                 onClick={() => signOut()}
-                className="rounded bg-blue-600 p-1 px-3 font-bold text-white transition-all hover:bg-blue-700"
+                className="rounded bg-blue-600 p-1 px-3 text-white transition-all hover:bg-blue-700"
               >
                 Sign out
               </button>
