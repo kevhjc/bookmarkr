2db81431c0d5d0307be95755044c420eab6aac78
Kevin Chung <63880429+kevhjc@users.noreply.github.com>
2022-02-04 10:39:32 -0800

    Remove unused imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,10 @@
-import { Fragment } from 'react'
-import { useSession, signIn, signOut } from 'next-auth/react'
 import Head from 'next/head'
-import Link from 'next/link'
-import { Menu, Popover, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/solid'
 
 import Container from '../components/Container'
 import Submit from '../components/Submit'
 import Bookmark from '../components/Bookmark'
 
 export default function Home() {
-  const { data: session } = useSession()
   const meta = {
     title: 'Bookmarkr',
     description: `Social Bookmarking`,
