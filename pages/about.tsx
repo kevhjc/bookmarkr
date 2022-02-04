bbf0eb91341c05f9aba5f6905b53c14b4d88c0ef
Kevin Chung <63880429+kevhjc@users.noreply.github.com>
2022-02-04 01:15:33 -0800

    Fix lint warnings

--- pages/about.tsx
+++ pages/about.tsx
@@ -7,22 +7,22 @@ export default function About() {
     <Container title="About – Bookmarkr">
       <div className="container mx-auto py-12 px-6">
         <h1 className="text-center text-4xl font-black leading-snug tracking-tight">
-          So what's Bookmarkr?
+          {`So what's Bookmarkr?`}
         </h1>
         <p className="mt-2 mb-6 px-6 text-center text-lg text-gray-700">
           Social bookmarking
         </p>
         <h1 className="text-center text-4xl font-black leading-snug tracking-tight">
-          What's the tech stack?
+          {`What's the tech stack?`}
         </h1>
         <p className="mt-2 mb-6 px-6 text-center text-lg text-gray-700">
-          Next.js, NextAuth.js, TailwindCSS, TypeScript, and PlanetScale
+          {`Next.js, NextAuth.js, TailwindCSS, TypeScript, and PlanetScale`}
         </p>
         <h1 className="text-center text-4xl font-black leading-snug tracking-tight">
-          Who made it?
+          {`Who made it?`}
         </h1>
         <p className="mt-2 mb-6 px-6 text-center text-lg text-gray-700">
-          Kevin H. Chung
+          {`Kevin H. Chung`}
         </p>
       </div>
     </Container>
