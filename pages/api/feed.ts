a1dd93058ab22af37766e17b9b57fc45b7339625
Kevin Chung <63880429+kevhjc@users.noreply.github.com>
2022-02-04 10:11:26 -0800

    Update API name

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import prisma from '../../lib/prisma'
 
-export default async function getFeed(
+export default async function assetHandler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
