c53656dd1d8fc0d382eeb83277b1f01af3fda166
Kevin Chung <63880429+kevhjc@users.noreply.github.com>
2022-02-05 00:03:36 -0800

    Update letter spacing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import Container from '../components/Container'
 import Submit from '../components/Submit'
 import Bookmark from '../components/Bookmark'
@@ -5,6 +7,9 @@ import Bookmark from '../components/Bookmark'
 export default function Home() {
   return (
     <Container>
+      <Head>
+        <title>Bookmarkr</title>
+      </Head>
       <Submit />
       <Bookmark />
     </Container>
