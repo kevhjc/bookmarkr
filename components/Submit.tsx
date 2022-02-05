7da616cd6701ab2166ca64f8c38c67b5e378c415
Kevin Chung <63880429+kevhjc@users.noreply.github.com>
2022-02-04 23:50:53 -0800

    Fix input margin

--- components/Submit.tsx
+++ components/Submit.tsx
@@ -58,13 +58,13 @@ export default function Submit() {
             disabled={session ? false : true}
             className={cn(
               session
-                ? 'mt-1 block w-full rounded-md bg-white px-4 py-2 pr-32 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-700'
-                : 'mt-1 block w-full rounded-md bg-gray-200 px-4 py-2 pr-32 dark:bg-gray-800'
+                ? 'mt-1 block w-full rounded-md bg-white px-4 py-2 pr-32 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-600'
+                : 'mt-1 block w-full rounded-md bg-gray-200 px-4 py-2 pr-32 dark:bg-gray-700'
             )}
           />
           {session ? (
             <button
-              className="w-22 absolute right-1 top-1 flex h-8 items-center justify-center rounded bg-gray-200 px-4 font-medium text-gray-700 transition-all hover:bg-gray-300 dark:bg-gray-300 dark:hover:bg-gray-400"
+              className="w-22 absolute right-1 top-1 flex h-8 items-center justify-center rounded bg-gray-200 px-4 font-medium text-gray-900 transition-all hover:bg-gray-300 dark:bg-gray-300 dark:hover:bg-gray-400"
               type="submit"
             >
               Submit
@@ -79,8 +79,8 @@ export default function Submit() {
             disabled={session ? false : true}
             className={cn(
               session
-                ? 'mt-1 block w-full rounded-md bg-white px-4 py-2 pr-32 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-700'
-                : 'mt-1 block w-full rounded-md bg-gray-200 px-4 py-2 pr-32 dark:bg-gray-800'
+                ? 'mt-2 block w-full rounded-md bg-white px-4 py-2 pr-32 text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:bg-gray-600'
+                : 'mt-2 block w-full rounded-md bg-gray-200 px-4 py-2 pr-32 dark:bg-gray-700'
             )}
             onKeyUp={() => (event ? onChangeCount(event) : 0)}
           />
