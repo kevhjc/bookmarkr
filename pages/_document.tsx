441f2a6af9d851f4cbceddfba121b1dd216e05e8
Kevin Chung <kevin.c@me.com>
2022-02-06 13:50:44 -0800

    Remove meta props

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
-  const meta = {
-    title: 'Bookmarkr',
-    description: `Social Bookmarking`,
-    image: 'https://bookmarkr.link/banner.png',
-    type: 'website',
-  }
-
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="shortcut icon" href="/favicon.ico" />
         <link
           rel="apple-touch-icon"
           href="/apple-touch-icon.png"
           sizes="180x180"
         />
-        <meta name="robots" content="follow, index" />
-        <meta content={meta.description} name="description" />
-        <meta property="og:title" content={meta.title} />
-        <meta property="og:site_name" content="Bookmarkr" />
-        <meta property="og:type" content={meta.type} />
-        <meta property="og:description" content={meta.description} />
-        <meta property="og:image" content={meta.image} />
       </Head>
       <body className="bg-white text-white dark:bg-neutral-900 dark:text-black">
         <Main />
