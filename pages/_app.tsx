25d77f974e72d143ecf4a12a78594e280332079a
Kevin Chung <kevin.c@me.com>
2022-02-06 00:28:25 -0800

    Fix global.css file name

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import { SessionProvider } from 'next-auth/react'
 
 import { ThemeProvider } from 'next-themes'
 import Layout from '../components/Layout'
-import '../styles/globals.css'
+import '../styles/global.css'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
