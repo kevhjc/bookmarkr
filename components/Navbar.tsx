fdfea9b6497318659dd1a04999f51ffeece82d55
Kevin Chung <kevin.c@me.com>
2022-02-06 23:55:58 -0800

    Update navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,6 +11,27 @@ interface INavProps {
   text: string
 }
 
+function LogoNavItem({ href, text }: INavProps) {
+  const router = useRouter()
+  const isActive = router.asPath === href
+
+  return (
+    <NextLink href={href}>
+      <a
+        className={cn(
+          isActive
+            ? 'font-black leading-snug tracking-tight text-gray-900 dark:text-gray-200'
+            : 'font-black leading-snug tracking-tight text-gray-800 dark:text-gray-300',
+          'flex items-center gap-2 rounded p-2 px-2 text-gray-900 transition-all hover:bg-blue-100 dark:text-gray-200 dark:hover:bg-blue-700'
+        )}
+      >
+        <Image src="/logo.svg" alt="Bookmarkr Logo" width={20} height={20} />
+        {text}
+      </a>
+    </NextLink>
+  )
+}
+
 function NavItem({ href, text }: INavProps) {
   const router = useRouter()
   const isActive = router.asPath === href
@@ -22,31 +43,10 @@ function NavItem({ href, text }: INavProps) {
           isActive
             ? 'dar:text-gray-200 font-medium text-gray-900'
             : 'font-medium text-gray-800 dark:text-gray-300',
-          'rounded p-2 px-3 text-gray-900 transition-all hover:bg-blue-100 dark:text-gray-200 dark:hover:bg-blue-700'
+          'rounded p-2 px-2 text-gray-900 transition-all hover:bg-blue-100 dark:text-gray-200 dark:hover:bg-blue-700'
         )}
       >
         <span>{text}</span>
-      </a>
-    </NextLink>
-  )
-}
-
-function LogoNavItem({ href, text }: INavProps) {
-  const router = useRouter()
-  const isActive = router.asPath === href
-
-  return (
-    <NextLink href={href}>
-      <a
-        className={cn(
-          isActive
-            ? 'font-bold leading-snug tracking-tight text-gray-900 dark:text-gray-200'
-            : 'font-bold leading-snug tracking-tight text-gray-800 dark:text-gray-300',
-          'flex items-center gap-2 rounded p-2 text-gray-900 transition-all hover:bg-blue-100 dark:text-gray-200 dark:hover:bg-blue-700'
-        )}
-      >
-        <Image src="/logo.svg" alt="Bookmarkr Logo" width={20} height={20} />
-        {text}
       </a>
     </NextLink>
   )
@@ -60,14 +60,14 @@ export default function Navbar() {
 
   return (
     <div className="mb-8 flex flex-wrap text-lg">
-      <div className="mb-4 w-full px-2 md:w-1/2 lg:w-1/2">
+      <div className="mb-4 w-full md:w-1/2 lg:w-1/2">
         <div className="md-justify-start flex h-12 items-center justify-center md:justify-start lg:justify-start">
           <nav className="space-x-4 md:block">
             <LogoNavItem href="/" text="Bookmarkr" />
           </nav>
         </div>
       </div>
-      <div className="w-full px-2 md:w-1/2 lg:w-1/2">
+      <div className="w-full md:w-1/2 lg:w-1/2">
         <div className="text-grey-dark flex h-12 items-center justify-center md:justify-end lg:justify-end">
           <nav className="space-x-4 md:block">
             <NavItem href="/about" text="About" />
@@ -75,7 +75,7 @@ export default function Navbar() {
               <button
                 aria-label="Sign in"
                 type="button"
-                className="rounded bg-blue-600 p-1 px-3 text-white transition-all hover:bg-blue-700 dark:bg-blue-700 dark:text-gray-200 dark:hover:bg-blue-900"
+                className="rounded bg-blue-500 p-1 px-2 font-medium text-gray-200 transition-all hover:bg-blue-700 dark:bg-blue-700 dark:text-gray-200 dark:hover:bg-blue-900"
                 onClick={() => signIn('github')}
               >
                 Sign in
@@ -84,7 +84,7 @@ export default function Navbar() {
               <button
                 aria-label="Sign out"
                 type="button"
-                className="rounded bg-blue-600 p-1 px-3 text-white transition-all hover:bg-blue-700 dark:bg-blue-700 dark:text-gray-200 dark:hover:bg-blue-900"
+                className="rounded p-1 px-2 font-medium text-gray-900 transition-all hover:bg-red-100 dark:bg-blue-700 dark:text-gray-200 dark:hover:bg-blue-900"
                 onClick={() => signOut()}
               >
                 Sign out
