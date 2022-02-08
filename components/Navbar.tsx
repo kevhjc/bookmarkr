6ca01aa5c1063a72ed013cb23b55122f38a4f360
Kevin Chung <kevin.c@me.com>
2022-02-07 16:03:19 -0800

    Update nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,7 +16,7 @@ function LogoNavItem({ href, text }: INavProps) {
     <NextLink href={href}>
       <a
         className={
-          'flex items-center gap-2 rounded p-1 px-2 font-bold text-gray-900 transition-all hover:bg-blue-100 dark:text-gray-200 dark:hover:bg-blue-700'
+          'flex items-center gap-2 rounded bg-blue-50 p-1 px-2 font-bold text-gray-900 transition-all hover:bg-blue-100 dark:bg-blue-400 dark:text-gray-200 dark:hover:bg-blue-700'
         }
       >
         <Image src="/logo.svg" alt="Bookmarkr Logo" width={20} height={20} />
