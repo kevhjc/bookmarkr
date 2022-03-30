1552314b61228d75c3cc2ee84587bc225c910244
Kevin Chung <kevin.c@me.com>
2022-03-29 21:12:04 -0700

    feat: add bookmark loading functionality

--- components/Bookmark.tsx
+++ components/Bookmark.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import useSWR from 'swr'
@@ -9,10 +10,16 @@ export default function Bookmark() {
   const fetcher = (url: string) => axios.get(url).then((res) => res.data)
   const { data: bookmarks, error } = useSWR('/api/bookmark', fetcher)
 
+  const [bookmarkNum, setBookmarkNum] = useState<number>(10)
+
+  const handleLoadMoreBookmarks = () => {
+    setBookmarkNum((prevbookmarkNum) => prevbookmarkNum + 10)
+  }
+
   if (!bookmarks)
     return (
       <FadeIn delay={200}>
-        <div className="mx-auto py-12 px-6">
+        <div className="px-6 py-12 mx-auto">
           <h1 className="flex items-center justify-center text-2xl font-black leading-snug text-neutral-900 dark:text-neutral-200">
             Loading...
           </h1>
@@ -22,7 +29,7 @@ export default function Bookmark() {
   if (error)
     return (
       <FadeIn delay={200}>
-        <div className="mx-auto py-12 px-6">
+        <div className="px-6 py-12 mx-auto">
           <h1 className="flex items-center justify-center text-2xl font-black leading-snug text-neutral-900 dark:text-neutral-200">
             Uh oh, something went wrong
           </h1>
@@ -32,29 +39,29 @@ export default function Bookmark() {
   return (
     <FadeIn>
       <div className="mt-4 space-y-8">
-        {bookmarks?.map((bookmark: any) => (
+        {bookmarks?.slice(0, bookmarkNum).map((bookmark: any) => (
           <div
             key={bookmark.id}
-            className="mx-auto mt-8 w-full transform gap-4 whitespace-normal rounded-lg border border-neutral-200 bg-neutral-50 p-1 transition-all duration-300 hover:shadow-sm dark:border-neutral-700 dark:bg-neutral-800/40"
+            className="w-full gap-4 p-1 mx-auto mt-8 whitespace-normal transition-all duration-300 transform border rounded-lg border-neutral-200 bg-neutral-50 hover:shadow-sm dark:border-neutral-700 dark:bg-neutral-800/40"
           >
-            <div className="mt-2 px-4 py-2">
+            <div className="px-4 py-2 mt-2">
               <h3>
                 <Link href={bookmark.url}>
                   <a
                     target="_blank"
                     rel="noopener noreferrer"
-                    className="break-words text-lg font-medium text-blue-500 underline hover:no-underline dark:text-blue-400"
+                    className="text-lg font-medium text-blue-500 underline break-words hover:no-underline dark:text-blue-400"
                   >
                     {bookmark.url}
                   </a>
                 </Link>
               </h3>
-              <div className="my-2 flex flex-row pt-2">
-                <p className="mb-2 max-w-2xl tracking-tight text-neutral-700 dark:text-neutral-200">
+              <div className="flex flex-row pt-2 my-2">
+                <p className="max-w-2xl mb-2 tracking-tight text-neutral-700 dark:text-neutral-200">
                   {bookmark.note}
                 </p>
               </div>
-              <div className="max-w-2x1 flex items-center gap-2 border-t pt-4 pb-2 font-mono text-sm tracking-tight text-neutral-500 dark:border-neutral-600 dark:text-neutral-400">
+              <div className="flex items-center gap-2 pt-4 pb-2 font-mono text-sm tracking-tight border-t max-w-2x1 text-neutral-500 dark:border-neutral-600 dark:text-neutral-400">
                 {bookmark.image ? (
                   <Image
                     alt="User Avatar"
@@ -71,6 +78,16 @@ export default function Bookmark() {
             </div>
           </div>
         ))}
+        {bookmarkNum < bookmarks.length ? (
+          <div className="flex justify-center">
+            <button
+              className="p-2 px-6 font-bold transition-all rounded cursor-pointer bg-neutral-200 text-neutral-600 hover:bg-neutral-300 dark:bg-neutral-700 dark:text-neutral-200 dark:hover:bg-neutral-800"
+              onClick={handleLoadMoreBookmarks}
+            >
+              &darr;
+            </button>
+          </div>
+        ) : null}
       </div>
     </FadeIn>
   )
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
-    <footer className="mx-auto mt-20 mb-2 flex w-full max-w-2xl flex-col items-center justify-center">
-      <hr className="border-1 mb-8 w-full border-neutral-200 dark:border-neutral-700" />
-      <span className="font-regular text-sm text-neutral-500 dark:text-neutral-400">
+    <footer className="flex flex-col items-center justify-center w-full max-w-2xl mx-auto mt-10 mb-2">
+      <hr className="w-full mb-8 border-1 border-neutral-200 dark:border-neutral-700" />
+      <span className="text-sm font-regular text-neutral-500 dark:text-neutral-400">
         Made with ❤️ in Los Angeles, CA
       </span>
     </footer>
