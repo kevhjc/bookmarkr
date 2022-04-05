8c0706775c3acdaecc8c877995ae1f5368d74a27
Kevin Chung <kevin.c@me.com>
2022-04-05 13:02:42 -0700

    refactor: update tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,25 @@
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  mode: 'jit',
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   darkMode: 'class',
   theme: {
+    extend: {
+      screens: {
+        dark: { raw: '(prefers-color-scheme: dark)' },
+      },
+    },
     fontFamily: {
       sans: ['Inter', 'sans-serif'],
       serif: ['Newsreader', 'serif'],
       mono: ['IBM Plex Mono', 'monospace'],
     },
-    extend: {},
   },
   plugins: [],
 }
